4dcf27e4b3c04af82d686b0a78732385814ecea4
Nidhish Lakhinana <47395841+nidhish1602@users.noreply.github.com>
2020-05-13 12:55:15 +0530

    Update 04.Student faculty DB.sql

--- 04.Student faculty DB.sql
+++ 04.Student faculty DB.sql
@@ -27,3 +27,40 @@ create TABLE faculty(
     deptid INT,
     PRIMARY key (fid)
 )
+
+insert into student values 
+(1,'john','cs','sr',19),
+(2,'smith','cs','jr',20), 
+(3,'jacob','cv','sr',20), 
+(4,'tom','cs','jr',20), 
+(5,'rahul','cs','jr',20),
+(6,'rita','cs','sr',21)
+
+insert into faculty values 
+(11,'harish',1000), 
+(12,'mv',1000), 
+(13,'mira',1001), 
+(14,'shiva',1002), 
+(15,'nupur',1000)
+
+insert into class values
+('class1','12/11/15 10:15:16','R1',14), 
+('class10','12/11/15 10:15:16','R128',14),
+('class2','12/11/15 10:15:20','R2',12), 
+('class3','12/11/15 10:15:25','R3',11), 
+('class4','12/11/15 20:15:20','R4',14), 
+('class5','12/11/15 20:15:20','R3',15), 
+('class6','12/11/15 13:20:20','R2',14), 
+('class7','12/11/15 10:10:10','R3',14) 
+
+insert into enrolled values 
+(1,'class1'), 
+(2,'class1'), 
+(3,'class3'), 
+(4,'class3'), 
+(5,'class4'), 
+(1,'class5'), 
+(2,'class5'), 
+(3,'class5'), 
+(4,'class5'), 
+(5,'class5') 
